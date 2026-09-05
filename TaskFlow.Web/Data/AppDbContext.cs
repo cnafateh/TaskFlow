91052acbba24fa7828c80ff200299e8ecf82e129
Sina <sinafateh1382@gmail.com>
2026-09-05 14:32:59 +0330

    feat: configure EF Core with SQLite

---
+++ TaskFlow.Web/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskFlow.Web.Models;
+
+namespace TaskFlow.Web.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<TaskItem> Tasks { get; set; }
+}
