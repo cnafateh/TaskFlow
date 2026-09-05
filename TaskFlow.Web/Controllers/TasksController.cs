38994dfd271d4605972054dbde936b14df04a4b8
Sina <sinafateh1382@gmail.com>
2026-09-05 11:01:52 +0330

    feat: add initial task MVC pages

---
+++ TaskFlow.Web/Controllers/TasksController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskFlow.Web.Controllers;
+
+public class TasksController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Details(int id)
+    {
+        ViewBag.TaskId = id;
+
+        return View();
+    }
+}
